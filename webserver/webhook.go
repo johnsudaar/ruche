359f10a7efba651191d05cc451799b91111c2a5d
Jonathan Hurter <john@scalingo.com>
2019-12-20 23:32:24 +0100

    Add model2 decoding

--- webserver/webhook.go
+++ webserver/webhook.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"context"
 	"encoding/hex"
 	"encoding/json"
 	"net/http"
@@ -34,7 +35,8 @@ type Value struct {
 }
 
 func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]string) error {
-	log := logger.Get(req.Context())
+	ctx := req.Context()
+	log := logger.Get(ctx)
 	config := config.Get()
 
 	var body Input
@@ -44,11 +46,13 @@ func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]stri
 		return errors.Wrap(err, "fail to decode body")
 	}
 
-	valueStr, err := hex.DecodeString(body.Value.Payload)
+	valueBuf, err := hex.DecodeString(body.Value.Payload)
 	if err != nil {
 		log.WithError(err).Error("fail to decode payload (hex)")
 		return errors.Wrap(err, "fail to decode payload (hex)")
 	}
+
+	valueStr := string(valueBuf)
 
 	values := make(map[string]interface{})
 	tags := make(map[string]string)
@@ -57,10 +61,14 @@ func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]stri
 	values["location_lon"] = body.Location.Lon
 	values["location_lat"] = body.Location.Alt
 
+	if valueStr == "Restart" {
+		log.Info("Restart: Ignoring...")
+		return nil
+	}
 	if len(valueStr) > 4 {
 		log.Info("Model 1 decoding")
 		// Model 1: Use only 1 value
-		value, err := strconv.ParseFloat(string(valueStr), 32)
+		value, err := strconv.ParseFloat(valueStr, 32)
 		if err != nil {
 			log.WithError(err).Error("fail to decode payload (str)")
 			return errors.Wrap(err, "fail to decode payload (str)")
@@ -68,6 +76,61 @@ func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]stri
 		values["temp"] = value
 	} else {
 		log.Info("Model 2 decoding")
+		// 0-1: Rucher ID
+		// 2-6: Temp xx.xx
+		// 7-11: Hum xx.xx
+		// 12-16: Lum xxxxx
+		// 17-21: Bat tension
+		// 22-26: Masse ruche1
+		// 27-31: Masse ruche1
+		// 32-36: Masse ruche1
+		// 37-41: Masse ruche1
+		ruchID, err := strconv.Atoi(valueStr[0:2])
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		temp, err := strconv.ParseFloat(valueStr[2:7], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		hum, err := strconv.ParseFloat(valueStr[7:12], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		lum, err := strconv.ParseFloat(valueStr[12:17], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		bat, err := strconv.ParseFloat(valueStr[17:22], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		ruch1, err := strconv.ParseFloat(valueStr[22:27], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		ruch2, err := strconv.ParseFloat(valueStr[27:32], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		ruch3, err := strconv.ParseFloat(valueStr[32:37], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+		ruch4, err := strconv.ParseFloat(valueStr[37:41], 64)
+		if err != nil {
+			return checkErr(ctx, err)
+		}
+
+		values["ruch_id"] = ruchID
+		values["temp"] = temp
+		values["hum"] = hum
+		values["lum"] = lum
+		values["bat"] = bat
+		values["ruch1"] = ruch1
+		values["ruch2"] = ruch2
+		values["ruch3"] = ruch3
+		values["ruch3"] = ruch4
 
 	}
 
@@ -99,4 +162,10 @@ func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]stri
 	log.Info("Done")
 
 	return nil
+}
+
+func checkErr(ctx context.Context, err error) error {
+	log := logger.Get(ctx)
+	log.WithError(err).Error(err.Error())
+	return err
 }
