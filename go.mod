6116c660b37928b7d2ba5a80a1e60dc1a616f77f
= <jonathan@hurter.fr>
2021-04-30 23:45:15 +0200

    Add new protocol version

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/johnsudaar/ruche
+
+go 1.16
+
+require (
+	github.com/Scalingo/go-handlers v1.2.6
+	github.com/Scalingo/go-utils v7.1.0+incompatible
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/influxdata/influxdb v1.7.9
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	golang.org/x/sys v0.0.0-20191220142924-d4481acd189f // indirect
+	gopkg.in/errgo.v1 v1.0.1
+)
--- webserver/webhook.go
+++ webserver/webhook.go
@@ -67,77 +67,75 @@ func Webhook(resp http.ResponseWriter, req *http.Request, params map[string]stri
 		log.Info("Restart: Ignoring...")
 		return nil
 	}
-	if len(valueStr) <= 5 {
-		log.Info("Model 1 decoding")
-		// Model 1: Use only 1 value
-		value, err := strconv.ParseFloat(valueStr, 32)
-		if err != nil {
-			log.WithError(err).Error("fail to decode payload (str)")
-			return errors.Wrap(err, "fail to decode payload (str)")
-		}
-		values["temp"] = value
-	} else {
-		log.Info("Model 2 decoding")
-		// 0-1: Rucher ID
-		// 2-6: Temp xx.xx
-		// 7-11: Hum xx.xx
-		// 12-16: Lum xxxxx
-		// 17-21: Bat tension
-		// 22-26: Masse hivee1
-		// 27-31: Masse hivee1
-		// 32-36: Masse hivee1
-		// 37-41: Masse hivee1
-		// 42-46: Sol tension
-		hiveID := hex.EncodeToString([]byte(valueStr[0:2]))
 
-		temp, err := strconv.ParseFloat(getValue(valueStr, 2, 7), 64)
-		if err != nil {
-			checkErr(ctx, err, "temp")
-		}
-		hum, err := strconv.ParseFloat(getValue(valueStr, 7, 12), 64)
-		if err != nil {
-			checkErr(ctx, err, "hum")
-		}
-		lum, err := strconv.ParseFloat(getValue(valueStr, 12, 17), 64)
-		if err != nil {
-			checkErr(ctx, err, "lum")
-		}
-		bat, err := strconv.ParseFloat(getValue(valueStr, 17, 22), 64)
-		if err != nil {
-			checkErr(ctx, err, "bat")
-		}
-		hive1, err := strconv.ParseFloat(getValue(valueStr, 22, 27), 64)
-		if err != nil {
-			checkErr(ctx, err, "hive1")
-		}
-		hive2, err := strconv.ParseFloat(getValue(valueStr, 27, 32), 64)
-		if err != nil {
-			checkErr(ctx, err, "hive2")
-		}
-		hive3, err := strconv.ParseFloat(getValue(valueStr, 32, 37), 64)
-		if err != nil {
-			checkErr(ctx, err, "hive3")
-		}
-		hive4, err := strconv.ParseFloat(getValue(valueStr, 37, 41), 64)
-		if err != nil {
-			checkErr(ctx, err, "hive4")
-		}
-		sol, err := strconv.ParseFloat(getValue(valueStr, 42, 47), 64)
-		if err != nil {
-			checkErr(ctx, err, "sol")
-		}
+	// 1-6: Temp sxx.xx
+	// 7-11: Hum xx.xx
+	// 12-17: Lum xxx.xx
+	// 18-21: Bat tension x.xx
+	// 22-25: Sol tension x.xx
+	// 26-31: Masse R1 xx.xxx
+	// 32-37: Masse R2 xx.xxx
+	// 38-43: Masse R3 xx.xxx
+	// 44-49: Masse R4 xx.xxx
+	// pour le futur, non implémenté
+	// 50-54: TempR1 xx.xx
+	// 55-59: TempR2 xx.xx
+	// 60-64: TempR3 xx.xx
+	// 65-69: TempR4 xx.xx
 
-
-		tags["hive_id"] = hiveID
-		values["temp"] = temp
-		values["hum"] = hum
-		values["lum"] = lum
-		values["bat"] = bat
-		values["hive_1"] = hive1
-		values["hive_2"] = hive2
-		values["hive_3"] = hive3
-		values["hive_4"] = hive4
-		values["sol"] = sol
+	values["temp"], err = strconv.ParseFloat(getValue(valueStr, 0, 6), 64)
+	if err != nil {
+		checkErr(ctx, err, "temp")
+	}
+	values["hum"], err = strconv.ParseFloat(getValue(valueStr, 6, 11), 64)
+	if err != nil {
+		checkErr(ctx, err, "hum")
+	}
+	values["lum"], err = strconv.ParseFloat(getValue(valueStr, 11, 17), 64)
+	if err != nil {
+		checkErr(ctx, err, "lum")
+	}
+	values["bat_tension"], err = strconv.ParseFloat(getValue(valueStr, 17, 21), 64)
+	if err != nil {
+		checkErr(ctx, err, "bat_tension")
+	}
+	values["sol_tension"], err = strconv.ParseFloat(getValue(valueStr, 21, 25), 64)
+	if err != nil {
+		checkErr(ctx, err, "sol_tension")
+	}
+	values["mass_r1"], err = strconv.ParseFloat(getValue(valueStr, 25, 31), 64)
+	if err != nil {
+		checkErr(ctx, err, "mass_r1")
+	}
+	values["mass_r2"], err = strconv.ParseFloat(getValue(valueStr, 31, 37), 64)
+	if err != nil {
+		checkErr(ctx, err, "mass_r2")
+	}
+	values["mass_r3"], err = strconv.ParseFloat(getValue(valueStr, 37, 43), 64)
+	if err != nil {
+		checkErr(ctx, err, "mass_r3")
+	}
+	values["mass_r4"], err = strconv.ParseFloat(getValue(valueStr, 43, 49), 64)
+	if err != nil {
+		checkErr(ctx, err, "mass_r4")
+	}
+	if len(valueStr) > 50 {
+		values["temp_r1"], err = strconv.ParseFloat(getValue(valueStr, 49, 54), 64)
+		if err != nil {
+			checkErr(ctx, err, "temp_r1")
+		}
+		values["temp_r2"], err = strconv.ParseFloat(getValue(valueStr, 54, 59), 64)
+		if err != nil {
+			checkErr(ctx, err, "temp_r2")
+		}
+		values["temp_r3"], err = strconv.ParseFloat(getValue(valueStr, 59, 64), 64)
+		if err != nil {
+			checkErr(ctx, err, "temp_r3")
+		}
+		values["temp_r4"], err = strconv.ParseFloat(getValue(valueStr, 64, 69), 64)
+		if err != nil {
+			checkErr(ctx, err, "temp_r4")
+		}
 	}
 
 	tags["stream_id"] = body.StreamID
